7563e1a932fcbf7032fb2cfed89386a747584618
leehoon <leehoon@kaist.ac.kr>
2019-05-26 15:19:21 +0900

    ;s

--- pymysql_tutorial_kh.py
+++ pymysql_tutorial_kh.py
@@ -5,7 +5,7 @@ import csv
 db = pymysql.connect(host='localhost',
                      port=3306,
                      user='root',
-                     passwd='',
+                     passwd='junmo12345',
                      db='joinclubkaist',
                      charset='utf8mb4')
 
